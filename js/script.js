eac2801fb7da4a7eb65fccd29bceae0e1cec00d1
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-07-10 18:21:34 +0530

    Modify .\js\script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+
   window.addEventListener("scroll", () => {
-    if (window.scrollY >50) {
-      navbar.classList.add("bg-dark");
+    if (window.scrollY > 50) {
+      if (!navbar.classList.contains("bg-dark")) {
+        navbar.classList.add("bg-dark");
+      }
       navbar.classList.add("navbar-sticky");
     } else {
-      navbar.classList.remove("bg-dark");
+      if (!navbar.classList.contains("toggled")) {
+        navbar.classList.remove("bg-dark");
+      }
       navbar.classList.remove("navbar-sticky");
     }
   });
 }
 
-document.addEventListener("DOMContentLoaded",userScroll)
+document.addEventListener("DOMContentLoaded", userScroll);
+
+const hamburger = document.querySelector('.navbar-toggler');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle("bg-dark");
+  navbar.classList.toggle("toggled");
+});
